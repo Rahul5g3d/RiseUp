7edb72846d81328e3bb2f55461b82630cafa6767
Dream Project <dreamproject@RAHULs-MacBook-Air.local>
2025-03-12 13:27:12 +0530

    Updated Vite project with new features

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NavBar from "./Components/Header";
+import NavBar from "./Components/NavBar";
 import Home from "./Components/Pages/Home";
 import LessonLearnt from "./Components/Pages/LessonLearnt";
 import DailyStudy from "./Components/Pages/DailyStudy";
